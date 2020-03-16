ee407f074efec18245c16b993a9ff5971b3e6b9b
YaseenFsharid <yaseen.sharid@gmail.com>
2020-03-16 14:11:48 +0200

    done

--- q2.js
+++ q2.js
@@ -3,9 +3,11 @@
 function drow() {
 
    for (var i=0 ; i<4 ; i++){
-       for(var j=0 ;j<=i ; j++){
+       for(var j=0 ;j <=i ; j++){
         console.log("*");
+        
        }
    } 
    
+}
 drow();
